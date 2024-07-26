484e65867dc4ae12266c11991700f89eff31f6cc
alsiam09 <156769180+alsiam09@users.noreply.github.com>
2024-07-26 16:21:50 +0600

    easyway

--- src/componat/Sarvis.jsx
+++ src/componat/Sarvis.jsx
@@ -3,16 +3,20 @@ import React from 'react'
 const Sarvis = () => {
     let Data = [
         {
-            title : "To-Let"
+            title : "To-Let",
+            subtitle : "Open"
         },
         {
-            title : "Car"
+            title : "Car",
+            subtitle : "close"
         },
         {
-            title : "Food"
+            title : "Food",
+            subtitle : "close"
         },
         {
-            title : "Shop"
+            title : "Shop",
+            subtitle : "close"
         },
     ]
   return (
@@ -21,8 +25,9 @@ const Sarvis = () => {
             <h2 className='p-[10px] flex-wrap w-[90%] lg:w-[100%] rounded-[10px] mx-auto bg-[#ffffffc4]'>eywuidwjkdsdhbwq nmghdscdghsdswqvdhwdfwjt</h2>
             <div className="box gap-3 flex flex-wrap w-[100%]  p-[20px]">
                 {Data.map((item)=>(
-                    <div className=" cursor-pointer justify-center items-center flex w-[48%] rounded-[15px] md:w-[200px] bg-[#ffffff] border-[3px] border-[#000000] h-[100px] packag">
+                    <div className=" relative cursor-pointer justify-center items-center flex w-[48%] rounded-[15px] md:w-[200px] bg-[#ffffff] border-[3px] border-[#000000] h-[100px] packag">
                         <h2 className='text-[#000] font-[600] text-[25px]'>{item.title}</h2>
+                        <span className='p-[3px] text-[#fff] font-[500] bg-[#ff1616] absolute top-0 right-0 rounded-[3px]'>{item.subtitle}</span>
                     </div>
                 ))}
             </div>
